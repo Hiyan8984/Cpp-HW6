affcc29c8d38609edc7a59178be5cc186812b1f6
Minjoon Kim <ww8985@naver.com>
2022-04-05 15:15:43 +0900

    3번 문제 추가

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -59,3 +59,84 @@ int main() {
 
 
 //문제2 
+/*
+class Person {
+	int id;
+	double weight;
+	string name;
+public:
+	//Person();
+	//Person(int id, string name);
+	Person(int id = 1, string name ="Grace", double weight = 20.5);
+	void show() { cout << id << ' ' << weight << ' ' << name << endl; }
+};
+*/
+/*
+Person::Person() {
+	this->id = 1;
+	this->name = "Grace";
+	this->weight = 20.5;
+}
+
+Person::Person(int id, string name) {
+	this->id = id;
+	this->name = name;
+	this->weight = 20.5;
+}
+*/
+/*
+Person::Person(int id, string name, double weight) {
+	this->id = id;
+	this->name = name;
+	this->weight = weight;
+}
+
+
+
+int main() {
+	Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
+	grace.show();
+	ashley.show();
+	helen.show();
+	system("pause");
+	return 0;
+}*/
+
+//문제3
+
+//3-1
+
+/*int big(int a, int b) {
+	int bigNum;
+	if (a > b) 
+		bigNum = a;
+	else 
+		bigNum = b;
+
+	if (bigNum > 100) 
+		return 100;
+	else
+		return bigNum;
+}
+
+int big(int a, int b, int c) {
+	int bigNum;
+	if (a > b)
+		bigNum = a;
+	else
+		bigNum = b;
+
+	if (bigNum > c)
+		return c;
+	else
+		return bigNum;
+}
+
+int main() {
+	int x = big(3, 5); // 3과 5중 큰 값 5는 최대값 100보다 작으므로, 5 리턴
+	int y = big(300, 60); // 300과 60중 큰 값 300이 최대값 100보다 크므로, 100 리턴
+	int z = big(30, 60, 50); // 30과 60 중 큰 값 60이 최대값 50보다 크므로, 50 리턴
+	cout << x << ' ' << y << ' ' << z << endl;
+	system("pause");
+	return 0;
+} */
